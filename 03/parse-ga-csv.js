375391772360b0e2c994878d261f7d93d9c350fb
tomof <fffjt007@gmail.com>
2019-05-08 17:11:14 +0900

    fix preview sub-command

--- 03/parse-ga-csv.js
+++ 03/parse-ga-csv.js
@@ -4,45 +4,26 @@ const path = require('path');
 const fs = require('fs');
 const program = require('commander');
 const jsYaml = require('js-yaml');
-
 const version = require('./package.json').version;
 const parseSessionByBrowser = require('./lib/parseSessionByBrowser');
 
-program
-  .version(version)
-  .usage('[options] <source csv paths ...>')
-  .option('-d, --dest <path>', 'Path of directory to write out converted json')
-  .option('--format [type]', 'Type of output format', /^(json|yaml)$/, 'json');
-
-program
-  .command('preview')
-  .description('Output parsed result in console')
-  .option('-s, --src <path>', 'Path of the source csv file path')
-  .action(options => {
-    // 解析した内容をコンソールログに表示
-    const csv = fs.readFileSync(options.src, 'utf-8');
-    const parsedData = parseSessionByBrowser(csv);
-    console.log(JSON.stringify(parsedData, null, 2));
-    process.exit(0);
-  });
-
-program.parse(process.argv);
-
-// 変換元のファイルを配列として取得
-const sourcePaths = program.args;
-if (sourcePaths.length === 0 || !program.dest) { // 必須のオプション引数値の検証
-  console.error('source csv paths and -d, --dest option are required!');
-  program.outputHelp();
-  process.exit(1);
-}
-
-const readFiles = (sourcePaths) => {
+const convertAbsoluteSourcePaths = (sourcePaths) => {
   return sourcePaths.map(sourcePath => {
-    return {
-      path: sourcePath,
-      content: fs.readFileSync(sourcePath, 'utf-8')
-    }
-  });
+    return path.resolve(process.cwd(), sourcePath);
+  })
+};
+
+const readCsvFiles = (sourcePaths) => {
+  try {
+    return sourcePaths.map(sourcePath => {
+      return {
+        path: sourcePath,
+        content: fs.readFileSync(sourcePath, 'utf-8')
+      }
+    });
+  } catch(e) {
+    throw new Error('Fail read csv file!');
+  }
 };
 
 const getDestPath = (sourcePath, formatType) => {
@@ -51,33 +32,78 @@ const getDestPath = (sourcePath, formatType) => {
 };
 
 const parseGACsv = (csvFiles, formatType) => {
+  try {
+    csvFiles.forEach(csvFile => {
+      const destPath = getDestPath(csvFile.path, formatType);
+      const parsedData = parseSessionByBrowser(csvFile.content);
+      const writeContent = formatType === 'yaml' ?
+        jsYaml.safeDump(parsedData) : JSON.stringify(parsedData, null, 2);
+      fs.writeFileSync(destPath, writeContent, 'utf-8');
+    });
+  } catch(e) {
+    throw new Error('Fail write file!');
+  }
+};
+
+const previewParsedGACsv = (csvFiles, previewFormatType) => {
   csvFiles.forEach(csvFile => {
-    const destPath = getDestPath(csvFile.path, formatType);
     const parsedData = parseSessionByBrowser(csvFile.content);
-    const writeContent = formatType === 'yaml' ?
+    const previewContent = previewFormatType === 'yaml' ?
       jsYaml.safeDump(parsedData) : JSON.stringify(parsedData, null, 2);
-    fs.writeFileSync(destPath, writeContent, 'utf-8');
+    console.log('');
+    console.log(csvFile.path);
+    console.log('----------------------------------------------------');
+    console.log(previewContent);
   });
 };
 
-// CSVファイルの読み込み
-let csvFiles;
-try {
-  csvFiles = readFiles(sourcePaths);
-}
-catch(e) {
-  // CSVファイル読み込み失敗時の処理
-  console.error('Fail read csv file!');
+program
+  .version(version)
+  .usage('[options] <sourceCsvPaths...>')
+  .option('-d, --dest <path>', 'Path of directory to write out converted json')
+  .option('--format [type]', 'Type of output format', /^(json|yaml)$/, 'json');
+
+program
+  .command('preview <sourceCsvPaths...>')
+  .description('Output parsed result in console')
+  .option('--preview-format [type]', 'Type of output format', /^(json|yaml)$/, 'json')
+  .action((sourceCsvPaths, options) => {
+    // previewサブコマンドの実行
+    const sourcePaths = convertAbsoluteSourcePaths(sourceCsvPaths);
+    if (sourcePaths.length === 0) { // 必須のオプション引数値の検証
+      console.error('sourceCsvPaths are required!');
+      program.outputHelp();
+      process.exit(1);
+    }
+    try {
+      const csvFiles = readCsvFiles(sourcePaths);
+      previewParsedGACsv(csvFiles, options.previewFormat);
+    }
+    catch(e) {
+      console.error(e.message);
+      program.outputHelp();
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+
+program.parse(process.argv);
+
+// コマンドの実行
+const sourcePaths = convertAbsoluteSourcePaths(program.args);
+if (sourcePaths.length === 0 || !program.dest) { // 必須のオプション引数値の検証
+  console.error('sourceCsvPaths and -d, --dest option are required!');
+  program.outputHelp();
   process.exit(1);
 }
 
-// 解析とファイルの書き出し
 try {
+  const csvFiles = readCsvFiles(sourcePaths);
   parseGACsv(csvFiles, program.format);
 }
 catch(e) {
-  // JSONファイル書き出し失敗時の処理
-  console.error('Fail write file!');
+  console.error(e.message);
+  program.outputHelp();
   process.exit(1);
 }
 
