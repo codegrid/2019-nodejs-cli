aff8699c8cea906558a3e33f04521b9f2234cd43
tomof <fffjt007@gmail.com>
2019-06-15 12:51:36 +0900

    第5回

---
+++ 05/md-converter.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const convertAbsolutePath = require('./lib/convert-absolute-path');
+const appConfig = require('./lib/read-config')();
+
+const argv = require('yargs')
+  .commandDir('commands')
+  .config(appConfig)
+  // パスを指定する各オプション値を絶対パスに変換
+  .coerce(['src', 'template', 'dest'], convertAbsolutePath)
+  // ポート番号が数値でなければエラーとする
+  .check(argv => {
+    if (isNaN(argv.port)) {
+      throw new Error('The specified port number is invalid!')
+    }
+    return true;
+  })
+  // エラーハンドリング
+  .fail((msg, err, yargs) => {
+    console.error('[ERROR]', err.message);
+    console.error('You should be doing', yargs.help());
+    process.exit(1);
+  })
+  .argv;
---
+++ 05/samples/sub/index.md
@@ -0,0 +1,26 @@
+# Subページ
+
+## リンク
+
+[Top](../index.html)
+[About](../about.html)
+
+## リスト
+
+- リスト1
+  - リスト1-1
+  - リスト1-2
+- リスト2
+  - リスト2-1
+
+1. First
+1. Second
+1. Third
+
+## テーブル
+
+| A列 | B列 |
+| --- | --- |
+| 1行目 | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ... |
+| 2行目 | Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. |
+| 3行目 | Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. |
